dfc941737defcec3fcfa8572d41475ccbac0c0f3
abhishek <abhiswaspatil1710@gmail.com>
2022-04-21 16:02:01 +0530

    updated pow of 3

--- function.py
+++ function.py
@@ -5,6 +5,8 @@ class Function:
         self.function_size = function_size
 
     def getLargestPackage(self):
-        if(len(self.function_imports)):
-            return {"", self.function_imports[0]}
-        return {"Do not Have any packages to import from lambda : ", None}
+        if(len(self.function_imports)>1):
+            return {"", self.function_imports[0],self.function_imports[1]}
+        elif(len(self.function_imports)==1):
+            return {"", self.function_imports[0],None}
+        return {"Do not Have any packages to import from lambda : ", None, None}
